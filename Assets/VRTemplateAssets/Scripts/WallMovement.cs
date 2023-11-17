660d99540346ed7beb7d434ccf838e02951015ff
yuihan <juyi@ualberta.ca>
2023-11-17 16:03:27 -0700

    move wall

--- Assets/VRTemplateAssets/Scripts/WallMovement.cs
+++ Assets/VRTemplateAssets/Scripts/WallMovement.cs
@@ -5,21 +5,15 @@ using UnityEngine;
 public class WallMovement : MonoBehaviour
 {
     public float movementSpeed = 5f; // Adjust the speed as needed
-    public float boundarySize = 20f; // Adjust the size of the boundary as needed
 
     void Update()
     {
         // Get input values for movement
-        float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         // Calculate the new position based on input and speed
         Vector3 newPosition = transform.position +
-            new Vector3(horizontalInput, verticalInput, 0f) * movementSpeed * Time.deltaTime;
-
-        // Clamp the position to stay within the square boundary
-        newPosition.x = Mathf.Clamp(newPosition.x, -boundarySize, boundarySize);
-        newPosition.y = Mathf.Clamp(newPosition.y, -boundarySize, boundarySize);
+            new Vector3(0f, verticalInput, 0f) * movementSpeed * Time.deltaTime;
 
         // Update the position of the wall
         transform.position = newPosition;

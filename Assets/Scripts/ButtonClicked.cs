5df61eb5b0b7bb2cc6f616050990b68357d2ed4a
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-11-17 20:48:25 -0700

    Connect script to button

--- Assets/Scripts/ButtonClicked.cs
+++ Assets/Scripts/ButtonClicked.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ButtonClicked : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,9 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OpenScene(string sceneName) {
+        SceneManager.LoadSceneAsync(sceneName);
     }
 }

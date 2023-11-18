ae676891d2355aeabec9b8d4d23b7dfaa5980441
hammad@ualberta.ca <mohammadhammad50@gmail.com>
2023-11-18 00:31:59 -0700

    Finished Flapping Mechanic (controller only)

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -1,39 +1,33 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
- 
- 
+
+// Referenced physics used here: https://gamedev.stackexchange.com/questions/70268/can-someone-explain-flappy-birds-physics-to-me
 public class Jump : MonoBehaviour
 {
     [SerializeField] private InputActionReference jumpButton;
-    [SerializeField] private float jumpHeight = 2.0f;
-    [SerializeField] private float gravityValue = -9.81f;
- 
- 
+    [SerializeField] private float fallingConstant = 2.0f;
+    [SerializeField] private float jumpSpeed = 0f;
+
+
+    private float vertSpeed = 0f;
     private CharacterController _characterController;
     private Vector3 _playerVelocity;
- 
+
     private void Awake() => _characterController = GetComponent<CharacterController>();
- 
+
     private void OnEnable() => jumpButton.action.performed += Jumping;
- 
+
     private void OnDisable() => jumpButton.action.performed -= Jumping;
- 
+
     private void Jumping(InputAction.CallbackContext obj)
     {
-        _playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        _playerVelocity.y = jumpSpeed;
     }
- 
+
     private void Update()
     {
-        if (_characterController.isGrounded && _playerVelocity.y < 0)
-        {
-            _playerVelocity.y = 0f;
-        }
- 
- 
-        _playerVelocity.y += gravityValue * Time.deltaTime;
+        _playerVelocity.y -= fallingConstant * Time.deltaTime;
         _characterController.Move(_playerVelocity * Time.deltaTime);
- 
     }
 }
